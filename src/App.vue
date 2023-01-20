90a769ed173cbb22ce9626825f173dd19945d795
Bryan <bryanstevenal@gmail.com>
2023-01-20 12:05:14 +0100

    Add header

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@ import PDFDocument from './components/PDFDocument.vue';
 </script>
 
 <template>
+    <header>
+        <h1>PDF VUEwer</h1>
+    </header>
     <main>
         <PDFDocument />
     </main>
