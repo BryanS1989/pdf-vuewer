31287059262a6e624aa9c598ef5ee6fa708d1dbb
Bryan <bryanstevenal@gmail.com>
2023-01-21 13:28:57 +0100

    Add currenPage prop to PDFDocument at App, and add styles

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@ export default {
     data() {
         return {
             url: 'https://cdn.filestackcontent.com/5qOCEpKzQldoRsVatUPS',
-            scale: 2,
+            scale: 1,
+            page: 3,
         };
     },
 };
@@ -16,13 +17,46 @@ export default {
 <template>
     <header>
         <h1>PDF Vuewer</h1>
+        <nav>
+            <input
+                type="number"
+                v-model="page"
+                min="1"
+            />
+        </nav>
     </header>
+    <hr />
     <main>
         <PDFDocument
             :url="url"
             :scale="scale"
+            :currentPage="page"
         />
     </main>
 </template>
 
-<style scoped></style>
+<style>
+header {
+    padding: 1rem 0;
+}
+
+nav {
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+    justify-content: space-evenly;
+    padding: 1rem;
+}
+
+main {
+    padding: 1rem 0;
+}
+
+input {
+    font-size: 1.2rem;
+    text-align: center;
+    border: none;
+    border-radius: 1rem;
+    padding: 0.5rem;
+}
+</style>
