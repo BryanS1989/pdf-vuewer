0b784ff21fe3de1ca6612e61910c8850d315f10b
Bryan <bryanstevenal@gmail.com>
2023-01-21 12:24:53 +0100

    Create PDFDocument component and use it on App component

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,26 @@
 import PDFDocument from './components/PDFDocument.vue';
 </script>
 
+<script>
+export default {
+    data() {
+        return {
+            url: 'https://cdn.filestackcontent.com/5qOCEpKzQldoRsVatUPS',
+            scale: 2,
+        };
+    },
+};
+</script>
+
 <template>
     <header>
-        <h1>PDF VUEwer</h1>
+        <h1>PDF Vuewer</h1>
     </header>
     <main>
-        <PDFDocument />
+        <PDFDocument
+            :url="url"
+            :scale="scale"
+        />
     </main>
 </template>
 
--- src/components/PDFDocument.vue
+++ src/components/PDFDocument.vue
@@ -1,5 +1,89 @@
-<script setup></script>
+<script>
+import { toRaw } from 'vue';
+import range from 'lodash/range';
+import * as pdfjs from 'pdfjs-dist/build/pdf';
+
+import PDFPage from './PDFpage.vue';
+
+export default {
+    name: 'PDFDocument',
+
+    components: {
+        PDFPage,
+    },
+
+    props: {
+        url: String,
+        scale: Number,
+    },
+
+    data() {
+        return {
+            pdf: undefined,
+            numPages: 0,
+            pages: [],
+        };
+    },
+
+    created() {
+        console.log('[PDFDocument] [created()]');
+
+        pdfjs.GlobalWorkerOptions.workerSrc =
+            '../../node_modules/pdfjs-dist/build/pdf.worker.min.js';
+
+        this.fetchPDF();
+    },
+
+    watch: {
+        pdf(pdf) {
+            console.log('[PDFDocument] [watch] [pdf()] pdf: ', pdf);
+
+            this.pages = [];
+
+            const promises = range(1, this.numPages + 1).map((number) => {
+                // INFO: Use toRaw because pdf is a Proxy, with toRaw we get:
+                // pdf  --> PDFDocumentProxy
+                return toRaw(pdf).getPage(number);
+            });
+
+            Promise.all(promises)
+                .then((pages) => {
+                    // pages    --> PDFPageProxy[]
+                    this.pages = pages;
+                })
+                .catch((err) => console.log(err));
+        },
+        url() {
+            this.fetchPDF();
+        },
+    },
+
+    methods: {
+        fetchPDF() {
+            console.log('[PDFDocument] [fetchPDF()]');
+
+            var loadingTask = pdfjs.getDocument(this.url);
+
+            loadingTask.promise
+                .then((pdf) => {
+                    this.pdf = pdf;
+                    this.numPages = pdf.numPages;
+                })
+                .catch((err) => console.log(err));
+        },
+    },
+};
+</script>
 
 <template>
     <h2>PDF Document</h2>
+    <div class="pdf-document">
+        <PDFPage
+            v-for="page in pages"
+            :key="page.pageNumber"
+            :page="page"
+            :scale="scale"
+            :pageNumber="page.pageNumber"
+        />
+    </div>
 </template>
