1e4714c2d3ebeae509c9dad2e91e729f93882025
Bryan <bryanstevenal@gmail.com>
2023-01-21 14:41:34 +0100

    Create buttons to change pages

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,18 @@ export default {
             page: 3,
         };
     },
+
+    methods: {
+        previousPage() {
+            console.log('[APP] [previousPage()]');
+            this.page -= 1;
+        },
+
+        nextPage() {
+            console.log('[APP] [nextPage()]');
+            this.page += 1;
+        },
+    },
 };
 </script>
 
@@ -18,11 +30,23 @@ export default {
     <header>
         <h1>PDF Vuewer</h1>
         <nav>
+            <button @click="previousPage()">
+                <font-awesome-icon
+                    icon="fa-solid fa-chevron-left"
+                    class="fa-xl"
+                />
+            </button>
             <input
                 type="number"
                 v-model="page"
                 min="1"
             />
+            <button @click="nextPage()">
+                <font-awesome-icon
+                    icon="fa-solid fa-chevron-right"
+                    class="fa-xl"
+                />
+            </button>
         </nav>
     </header>
     <hr />
@@ -58,5 +82,24 @@ input {
     border: none;
     border-radius: 1rem;
     padding: 0.5rem;
+}
+
+button {
+    border: none;
+    background-color: transparent;
+    color: unset;
+    cursor: pointer;
+}
+
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+}
+
+/* Firefox */
+input[type='number'] {
+    -moz-appearance: textfield;
 }
 </style>
