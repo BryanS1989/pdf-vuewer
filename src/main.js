59bc9564132009662ebbb3d694ab45e19d6f2a49
Bryan <bryanstevenal@gmail.com>
2023-01-21 14:41:16 +0100

    Install and setup Fontawesome

--- src/main.js
+++ src/main.js
@@ -3,6 +3,25 @@ import App from './App.vue';
 
 import './assets/main.css';
 
+// FONTAWESOME
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* import specific icons */
+import {
+    faChevronLeft,
+    faChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
+
+/* add icons to the library */
+library.add(faChevronLeft, faChevronRight);
+
 const app = createApp(App);
+
+/* add font awesome icon component */
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount('#app');
