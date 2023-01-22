0a044d3a7cd0755f2798b77c3c6f028f12fd17b7
Bryan <bryanstevenal@gmail.com>
2023-01-22 13:18:04 +0100

    Add numpages to App, methods zoom in and zoom out

--- src/App.vue
+++ src/App.vue
@@ -8,53 +8,131 @@ export default {
         return {
             url: 'https://cdn.filestackcontent.com/5qOCEpKzQldoRsVatUPS',
             scale: 1,
-            page: 3,
+            page: undefined,
+            numPages: 1,
         };
     },
 
+    computed: {
+        theScale() {
+            return +this.scale.toFixed(2);
+        },
+    },
+
     methods: {
+        checkPage() {
+            if (this.page !== undefined) {
+                if (this.page <= 1) {
+                    this.page = 1;
+                } else if (this.page >= this.numPages) {
+                    this.page = this.numPages;
+                }
+            } else {
+                this.page = 'all';
+            }
+        },
+
         previousPage() {
             console.log('[APP] [previousPage()]');
-            this.page -= 1;
+            if (this.page === undefined || this.page <= 1) {
+                this.page = 1;
+            } else {
+                this.page -= 1;
+            }
         },
 
         nextPage() {
             console.log('[APP] [nextPage()]');
-            this.page += 1;
+            if (this.page === undefined) {
+                this.page = 1;
+            } else if (this.page >= this.numPages) {
+                this.page = this.numPages;
+            } else {
+                this.page += 1;
+            }
+        },
+
+        zoomOut() {
+            console.log('[APP] [zoomOut()] scale: ', this.theScale);
+            if (this.theScale > 0.2) {
+                this.scale -= 0.2;
+            }
+        },
+
+        zoomIn() {
+            console.log('[APP] [zoomIn()]');
+            if (this.theScale < 3) {
+                this.scale += 0.2;
+            }
         },
     },
 };
 </script>
 
 <template>
-    <header>
+    <header class="">
         <h1>PDF Vuewer</h1>
-        <nav>
-            <button @click="previousPage()">
+
+        <div class="flex flex-grow-1">
+            <label class="flex cursor_pointer">
                 <font-awesome-icon
-                    icon="fa-solid fa-chevron-left"
+                    icon="fa-solid fa-file-arrow-up"
                     class="fa-xl"
                 />
-            </button>
-            <input
-                type="number"
-                v-model="page"
-                min="1"
-            />
-            <button @click="nextPage()">
-                <font-awesome-icon
-                    icon="fa-solid fa-chevron-right"
-                    class="fa-xl"
+                <h2>Upload File</h2>
+                <input
+                    type="file"
+                    accept="application/pdf"
+                    hidden
                 />
-            </button>
-        </nav>
+            </label>
+
+            <nav class="flex flex-grow-1">
+                <button @click="previousPage()">
+                    <font-awesome-icon
+                        icon="fa-solid fa-chevron-left"
+                        class="fa-xl"
+                    />
+                </button>
+                <input
+                    type="number"
+                    v-model="page"
+                    @keyup="checkPage()"
+                    min="1"
+                    :max="numPages"
+                    :placeholder="page === undefined ? 'ALL' : ''"
+                />
+                <button @click="nextPage()">
+                    <font-awesome-icon
+                        icon="fa-solid fa-chevron-right"
+                        class="fa-xl"
+                    />
+                </button>
+            </nav>
+
+            <div class="flex">
+                <button @click="zoomOut()">
+                    <font-awesome-icon
+                        icon="fa-solid fa-magnifying-glass-minus"
+                        class="fa-xl"
+                    />
+                </button>
+                <button @click="zoomIn()">
+                    <font-awesome-icon
+                        icon="fa-solid fa-magnifying-glass-plus"
+                        class="fa-xl"
+                    />
+                </button>
+            </div>
+        </div>
     </header>
     <hr />
-    <main>
+    <main class="flex flex-col overflow--auto">
         <PDFDocument
             :url="url"
-            :scale="scale"
+            :scale="theScale"
             :currentPage="page"
+            @num-pages="(pages) => (this.numPages = pages)"
         />
     </main>
 </template>
@@ -64,23 +142,29 @@ header {
     padding: 1rem 0;
 }
 
-nav {
-    display: flex;
-    flex-flow: row nowrap;
-    align-items: center;
-    justify-content: space-evenly;
-    padding: 1rem;
-}
-
 main {
     padding: 1rem 0;
+}
+
+main > * {
+    padding-bottom: 1rem;
 }
 
 input {
     font-size: 1.2rem;
     text-align: center;
-    border: none;
+    padding: 0.5rem;
     border-radius: 1rem;
+    border: 0.5px solid var(--color-text);
+    background-color: transparent;
+    color: var(--color-text);
+}
+
+input::placeholder {
+    color: var(--color-text);
+}
+
+label {
     padding: 0.5rem;
 }
 
@@ -89,6 +173,11 @@ button {
     background-color: transparent;
     color: unset;
     cursor: pointer;
+    text-align: center;
+}
+
+svg {
+    padding: 0.5rem;
 }
 
 /* Chrome, Safari, Edge, Opera */
